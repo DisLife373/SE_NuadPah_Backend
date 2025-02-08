7b38b782cf7d7dea5e8a3829f188c448bf260004
DisLife373 <keerataphant@gmail.com>
2025-02-08 20:46:48 +0700

    fixbug3

--- src/handler/auth/handleForgetPW.ts
+++ src/handler/auth/handleForgetPW.ts
@@ -8,17 +8,23 @@ export const handleForgetPW = async (
   reply: FastifyReply,
   app: FastifyInstance
 ) => {
-  const emailForget = request.body.email;
-  const client = await app.pg.connect();
-  const { rows } = await client.query(
-    `
+  try {
+    const emailForget = request.body.email;
+    const client = await app.pg.connect();
+    const userQuery = await client.query(
+      `
       SELECT * FROM public."User"
       WHERE email = $1;
     `,
-    [emailForget]
-  );
+      [emailForget]
+    );
 
-  if (rows.length == 1) {
+    if (userQuery.rows.length != 1) {
+      return reply.status(404).send({
+        message: "Email is not found",
+      });
+    }
+
     const otp = crypto.randomInt(1000, 9999).toString();
     const expiresAt = new Date(Date.now() + 1 * 60 * 1000); // (Date.now() + minute(s) * toMinute * toSec)
     const { rows } = await client.query(
@@ -34,5 +40,8 @@ export const handleForgetPW = async (
     return reply.status(201).send({
       message: "OTP is already sent, Please check your Email",
     });
+  } catch (e) {
+    console.error(e);
+    return reply.status(500).send({ message: "Internal Server Error" });
   }
 };
--- src/handler/auth/handleResetPW.ts
+++ src/handler/auth/handleResetPW.ts
@@ -7,23 +7,27 @@ export const handleResetPW = async (
   reply: FastifyReply,
   app: FastifyInstance
 ) => {
-  const { email, newpw, confirmpw } = request.body;
+  try {
+    const { email, newpw, confirmpw } = request.body;
 
-  if (newpw != confirmpw) {
-    return reply.status(403);
-  }
+    if (newpw != confirmpw) {
+      return reply.status(403);
+    }
 
-  const hashedPW = await hashPassword(newpw);
+    const hashedPW = await hashPassword(newpw);
 
-  const client = await app.pg.connect();
-  const { rows } = await client.query(
-    `
+    const client = await app.pg.connect();
+    const { rows } = await client.query(
+      `
         UPDATE public."User"
         SET password = $1
         WHERE email = $2;
     `,
-    [hashedPW, email]
-  );
+      [hashedPW, email]
+    );
 
-  return reply.status(200).send({ message: "Password is updated" });
+    return reply.status(200).send({ message: "Password is updated" });
+  } catch (e) {
+    return reply.status(500).send({ message: "Internal Server Error" });
+  }
 };
