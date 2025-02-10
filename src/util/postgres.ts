689c3a89a953d4105b6c29edaa6ae8de7f7716c0
DisLife373 <keerataphant@gmail.com>
2025-02-10 14:22:11 +0700

    Bug Fix9

--- src/util/postgres.ts
+++ src/util/postgres.ts
@@ -2,7 +2,11 @@ import { Pool } from "pg";
 import config from "../config/config";
 
 const pool = new Pool({
-  connectionString: config.db,
+  user: config.user,
+  host: config.host,
+  database: config.db_name,
+  password: config.password,
+  port: 5432,
 });
 
 pool.on("connect", () => {
