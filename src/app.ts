8da83e163da837417cdd4995d24ddf56ae0a133d
DisLife373 <keerataphant@gmail.com>
2025-02-10 12:13:30 +0700

    Fix Bug7

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import fastify, { FastifyServerOptions } from "fastify";
 import fastifyPostgres, { PostgresPluginOptions } from "@fastify/postgres";
+import fastifyCors from "@fastify/cors";
 import config from "./config/config";
 import authRouter from "./router/auth";
 import massageRouter from "./router/massage";
@@ -7,6 +8,12 @@ import adminRouter from "./router/admin";
 
 const buildApp = (options: FastifyServerOptions) => {
   const app = fastify(options);
+
+  // Register CORS plugin
+  app.register(fastifyCors, {
+    origin: "*", // Allow all origins. Adjust this as needed for your use case.
+    methods: ["GET", "POST", "PUT", "DELETE"], // Allowed HTTP methods
+  });
 
   const postgresOptions: PostgresPluginOptions = {
     connectionString: config.db,
@@ -16,6 +23,10 @@ const buildApp = (options: FastifyServerOptions) => {
   app.register(authRouter, { prefix: "/auth" });
   app.register(massageRouter, { prefix: "/massage" });
   app.register(adminRouter, { prefix: "/admin" });
+
+  app.ready(() => {
+    app.printRoutes();
+  });
 
   app.setErrorHandler((error, request, reply) =>
     reply.status(error.statusCode || 500).send({
